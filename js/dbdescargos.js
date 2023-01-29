9597424b58a08a66177cbf86a48160362e9a06a0
Rodrigo Muñoz <rodrigo.munoz@codein.coop.ar>
2023-01-29 00:20:28 -0300

    Actualizacion para Documentos de Descargos

--- js/dbdescargos.js
+++ js/dbdescargos.js
@@ -1,65 +1,64 @@
-// ESTE DOCUMENTO ESTÁ EN ELABORACION AL 22/01/2023 17:40HS
-// Creo un objeto DOCUMENTO
-class descargo {
-    constructor(id, cuil, idCausa, email, domicilioReal, mensaje, archivo, fechaHoy) {
-        this.id = id;
-        this.cuil = cuil;        
+const baseDocumentos = [];
+const documentosEnUnaCausa = [];
+
+class documento {
+    constructor(idCausa, cuil, email, domicilioReal, mensaje, tipoDocumento, archivo, fechaEnvio) {
         this.idCausa = idCausa;
+        this.cuil = cuil;
         this.email = email;
-        this.domicioReal = domicilioReal;
+        this.domicilioReal = domicilioReal;
         this.mensaje = mensaje;
-        this.link = link;
+        this.tipoDocumento = tipoDocumento;
+        this.archivo = archivo;
         this.fechaEnvio = fechaEnvio;
     };
-}
 
-//Busco crear un ARRAY con multiples OBJETOS(documentos) y sus métodos
-class dbDescargos {
-    baseDescargos = [];
-    
-    agregar(id, cuil, idCausa, email, domicilioReal, mensaje, archivo, fechaHoy){
-        this.baseDescargos.push(new descargo(id, cuil, idCausa, email, domicilioReal, mensaje, archivo, fechaHoy));
-    };
-
-    imprimir(){
-        this.baseDescargos.forEach(element => {
-            console.log(element);
-        });
+    guardarDescargo(documento) {
+        baseDocumentos.push(documento);
     }
 }
 
-const guardarDescargoLocalStorage = (id,descargo) => {
-    localStorage.setItem(id, JSON.stringify(descargo));
+const buscarDescargosCausa = (idCausa) => {
+    let baseDB = baseDocumentos;
+    for (let documento of baseDB) {
+        if (documento.idCausa === idCausa) {
+            documentosEnUnaCausa.push(documento)
+        }
+    }
 }
 
-//Busco los antecedentes guardados en LOCAL STORAGE
-const buscarDescargoLocalStorage = function(idCausa, cuil) {
-    return JSON.parse(localStorage.getItem(idCausa, cuil));
-}
-
-guardarDescargoLocalStorage();
-
-
-const guardarEnvioDescargo = (cuil, idCausa) =>{
-    console.log("Entró al proceso para almacenar información de Descargo.!");
-    let email = document.getElementById("formEmail").value;
-    let domicilioReal = document.getElementById("formDomicilioReal").value;
-    let mensaje = document.getElementById("formMensaje").value;
-    let archivo = document.getElementById("formArchivo").value;
-
-    console.log(email);
-    console.log(domicilioReal);
-    console.log(mensaje);
-    console.log(archivo);
-    console.log("Busco causas del CUIL: "+usuario());
-    console.log("Busco causa ID NRO.: "+IdCausaActiva());
-
-    let fechaHoy = new Date();
+const eliminarDescargo = (idCausa, cuil) => {
+    let posicion = documentosEnUnaCausa.findIndex(eliminar =>
+         ((eliminar.id === id) && (eliminar.idCausa === idCausa) && (eliminar.cuil === cuil)))
     
-    //let documentoRecibido = dbDescargos.agregar(1, usuario(), idCausa, email, domicilioReal, mensaje, archivo, fechaHoy);
-    //console.log(documentoRecibido);
+    if (posicion > 0) {
+        documentosEnUnaCausa.splice(posicion,1);
+    }
 }
 
+const causaSeleccionada = () =>{
+    return JSON.parse(sessionStorage.getItem("cauSelect"));
+};
 
+const enviarDescargo = () => {
+    const formulario = document.getElementById("enviaDescargo");
+    formulario.onsubmit = (event) => {
+        event.preventDefault();
+        let email = document.getElementById("formEmail").value;
+        let domicilioReal = document.getElementById("formDomicilioReal").value;
+        let mensaje = document.getElementById("formMensaje").value;
+        let tipoDocumento = document.getElementById("tipoContacto").value;
+        let archivo = document.getElementById("formArchivo").value;
+        let fechaEnvio = new Date();
+        enviarFormulario(email, domicilioReal, mensaje, tipoDocumento, archivo, fechaEnvio);
+    }
+    console.log("Opero con el usuario CUIL: "+usuario());
+    console.log("Incorporaré un contacto en causa ID NRO.: ", causaSeleccionada());
+}
 
-guardarEnvioDescargo();
+function enviarFormulario(email, domicilioReal, mensaje, tipoDocumento, archivo, fechaEnvio) {
+    let almacenar = new documento(causaSeleccionada(),usuario(),email, domicilioReal, mensaje, tipoDocumento, archivo, fechaEnvio);
+    almacenar.guardarDescargo(almacenar);
+}
+
+enviarDescargo();
--- js/global.js
+++ js/global.js
@@ -3,7 +3,4 @@ const usuario = () =>{
     return JSON.parse(sessionStorage.getItem("cuil"));
 };
 
-// Obtengo el ID CAUSA SELECCIONADA
-const IdCausaActiva = () =>{
-    return JSON.parse(sessionStorage.getItem("idCausaSeleccionada"));
-};
+
